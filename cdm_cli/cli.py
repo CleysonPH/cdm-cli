6f765864339d61244b7984224b2a8263a15dcf94
Cleyson Lima <cleysonph@gmail.com>
2020-04-10 01:08:21 -0300

    Add a flag for show the cli version

--- cdm_cli/cli.py
+++ cdm_cli/cli.py
@@ -7,9 +7,15 @@ from .finder import Finder, Manga
 from .exeptions import ChapterNotFound, MangaNotFound
 
 
-@click.group()
-def cli():
-    pass
+@click.group(invoke_without_command=True)
+@click.option('--version', is_flag=True, help='Shows the version of the CLI')
+def cli(version: bool):
+    if version:
+        from cdm_cli import __version__
+        click.echo(f'Version {__version__}')
+    else:
+        ctx = click.get_current_context()
+        click.echo(ctx.get_help())
 
 
 @click.argument('manga')
